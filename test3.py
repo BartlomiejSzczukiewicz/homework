f0b38363afb6bb5ed95d909f7ebb75f3d405c394
Bartłomiej Szczukiewicz <halny125@gmail.com>
2023-10-10 19:54:23 +0200

    homework1

---
+++ test3.py
@@ -0,0 +1,15 @@
+width = 17
+height = 12.0
+
+width = width/2
+print(width)
+print(type(width))
+width = width/2.0
+print(width)
+print(type(width))
+height = height/3
+print(height)
+print(type(height))
+number = 1+2*5
+print(number)
+print(type(number))
